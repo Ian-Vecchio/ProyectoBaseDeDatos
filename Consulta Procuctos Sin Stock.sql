97be3cb50ddf92c4e6b586dac4fe870c3fa3d6fd
santetem <santiago.diazet32@gmail.com>
2026-05-20 10:47:01 -0300

    Add files via upload

---
+++ Consulta Procuctos Sin Stock.sql
@@ -0,0 +1,7 @@
+-- consulta ver productos sin stock 
+create view vista_productos_sin_stock as
+select pub.id_publicacion, p.nombre as producto, p.precio, u.nombre_usuario as vendedor
+from publicaciones pub
+join productos p on pub.id_producto = p.id_producto
+join usuarios u on pub.id_usuario = u.id_usuario
+where pub.stock = 0;
