e33445d22fc0ce3c9a2de23c15ce029dd72473a1
santetem <santiago.diazet32@gmail.com>
2026-05-28 16:15:20 -0300

    Delete Consulta Ver Carrito.sql

--- Consulta Ver Carrito.sql
+++
@@ -1,8 +0,0 @@
--- consulta ver carrito
-create view ver_carrito as
-select dc.id_carrito, u.nombre_usuario as comprador, p.nombre as producto, dc.cantidad, dc.precio as precio_unitario, (dc.cantidad * dc.precio) as total_producto
-from detalle_carro dc
-join carritos c on dc.id_carrito = c.id_carrito
-join usuarios u on c.id_comprador = u.id_usuario
-join publicaciones pub on dc.id_publicacion = pub.id_publicacion
-join productos p on pub.id_producto = p.id_producto;
