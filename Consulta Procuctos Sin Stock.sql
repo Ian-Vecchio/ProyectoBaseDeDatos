7e9a1f62184c46541047d791edce18521e89388a
santetem <santiago.diazet32@gmail.com>
2026-05-28 16:15:18 -0300

    Delete Consulta Procuctos Sin Stock.sql

--- Consulta Procuctos Sin Stock.sql
+++
@@ -1,7 +0,0 @@
--- consulta ver productos sin stock 
-create view productos_sin_stock as
-select pub.id_publicacion, p.nombre as producto, p.precio, u.nombre_usuario as vendedor
-from publicaciones pub
-join productos p on pub.id_producto = p.id_producto
-join usuarios u on pub.id_usuario = u.id_usuario
-where pub.stock = 0;
