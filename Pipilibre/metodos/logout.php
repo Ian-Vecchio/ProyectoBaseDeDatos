4f75f1fa633e5250b838c7cd77cdbb936855ade6
santetem <santiago.diazet32@gmail.com>
2026-06-08 14:28:56 -0300

    Delete Pipilibre directory

--- Pipilibre/metodos/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start(); 
-session_destroy(); 
-
-session_start(); 
-$_SESSION ['trigger_logout'] = 'pesnequelahabia olvidado turun tun';
-header("Location: ../Vistas/inicio/inicio.php");
-?>
